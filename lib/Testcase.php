a4f7238fd071d89a72dd9c2ba752499e2f9ea2a5
Donatas Olsevičius <donatas.olsevicius@proton.ch>
2023-01-07 21:56:31 +0100

    env setup

---
+++ lib/Testcase.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\Assert;
+
+abstract class Testcase extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider dataProvider
+     */
+    public function testSolution($expected, ...$params): void
+    {
+        $result = $this->getMethodToTest()(...$params);
+
+        Assert::assertEquals($expected, $result);
+    }
+
+    abstract protected function getMethodToTest(): callable;
+
+    abstract protected function dataProvider(): iterable;
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+error_reporting(E_ALL);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+ini_set('xdebug.max_nesting_level', '128');
+ini_set('max_execution_time', '0');
+
+date_default_timezone_set('UTC');
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
